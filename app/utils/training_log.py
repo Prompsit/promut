07488a1f02f505bce425dcb3708bcc05de779389
Tudor <tudornm@prompsit.com>
2025-01-31 15:43:27 +0100

    Update training log regex matches for Marian logs

--- app/utils/training_log.py
+++ app/utils/training_log.py
@@ -1,6 +1,27 @@
 import re
 
-# Regex for training log analysis
-training_regex = r'^(\d{4}-\d{2}-\d{2}) (\d{2}:\d{2}:\d{2}),\d+\s+(\-\s+\w+\s)*(-\s+)*Epoch\s+(\d+),?\sStep:\s+(\d+),?\s+Batch Loss:\s+(\d+.\d+),?\s+Tokens per Sec:\s+(\d+),\s+Lr:\s+(\d+.\d+)$'
-validation_regex = r'^(\d{4}-\d{2}-\d{2})\s+(\d{2}:\d{2}:\d{2},\d+)\s(\w|\s|\(|\)|\-)+(\d+),\s+step\s*(\d+):\s+bleu:\s+(\d+\.\d+),\s+loss:\s+(\d+\.\d+),\s+ppl:\s+(\d+\.\d+),\s+duration:\s+(\d+.\d+)s$'
 re_flags = re.IGNORECASE | re.UNICODE
+
+# manual regex matching with groups for training:
+# 0: date of current update
+# 1: timestamp of current update
+# 2: "Ep." match for epochs
+# 3: epoch number
+# 4: update number
+# 5: sentences read in current epoch
+# 6: loss function result for current update
+# 7: time between updates
+# 8: words per second read between updates
+# 9: gNorm value
+# 10: learning rate value for current update
+training_regex = r'^\[(\d{4}-\d{2}-\d{2})\s(\d{2}:\d{2}:\d{2})\]\s(Ep)\.\s(\d+)\s:\sUp\.\s(\d+)\s:\sSen\.\s(\d+,\d+)\s:\sCost\s(\d+\.\d+)\s:\s' \
+                    'Time\s(\d+\.\d+)\w\s:\s(\d+\.\d+)\swords/s\s:\sgNorm\s(\d+\.\d+)\s:\sL\.r\.\s(\d+\.\d+\w?-?\d+)$'
+
+# manual regex matching with groups for validation hardcoded for bleu:
+# 0: date of current update
+# 1: timestamp of current validation step
+# 2: validation flag "[valid]"
+# 3: epoch number
+# 4: update number
+# 5: bleu score
+validation_regex = r'\[(\d{4}-\d{2}-\d{2})\s(\d{2}:\d{2}:\d{2})\]\s\[(valid)\]\sEp\.\s(\d+)\s:\sUp\.\s(\d+)\s:\s(\w+(?:-\w+)*)\s:\s(\d+\.?\d*).*$'
